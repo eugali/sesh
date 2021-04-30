6278071fb9da19d9da422929432bded7a47e13cc
Jake Spracher <jakespracher@gmail.com>
2021-04-30 16:30:56 -0400

    Implement API layer first methods

--- App.tsx
+++ App.tsx
@@ -1,25 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
-import useCachedResources from './hooks/useCachedResources';
-import useColorScheme from './hooks/useColorScheme';
-import Navigation from './navigation';
+import useCachedResources from "./hooks/useCachedResources";
+import useColorScheme from "./hooks/useColorScheme";
+import Navigation from "./navigation";
 
-import firebase from "firebase/app"
+import firebase from "firebase/app";
 import "firebase/firestore";
-import db from "./shared/db"
-
-// Initialize Firebase
-const firebaseConfig = {
-  apiKey: "AIzaSyBRb4_6tE_OJv7GbORE0BcugAasAJR2Lv0",
-  authDomain: "sesh-e5398.firebaseapp.com",
-  projectId: "sesh-e5398",
-  storageBucket: "sesh-e5398.appspot.com",
-  messagingSenderId: "526817755196",
-  appId: "1:526817755196:web:11f6e3c174177ed80d8339",
-  measurementId: "G-GQN94NFWQT"
-};
+import { firebaseConfig } from "./constants/Config";
+import db from "./shared/db";
 
 firebase.initializeApp(firebaseConfig);
 
