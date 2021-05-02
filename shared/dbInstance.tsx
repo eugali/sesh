b9e0214baeffc4a0b1d8eda176ff1a3fccf3e527
Jake Spracher <jakespracher@gmail.com>
2021-05-02 10:46:35 -0400

    Clean up participant count UX
    Dont show start button unless enough
    Show expected count next to participants
    Add constants to config for thesse

--- shared/dbInstance.tsx
+++ shared/dbInstance.tsx
@@ -1,11 +1,21 @@
 import db from "./db";
 import firebase from "firebase/app";
 import "firebase/firestore";
-import { firebaseConfig } from "../constants/Config";
+import {
+  firebaseConfig,
+  prodCollectionName,
+  MinParticipants,
+  MaxVotes,
+} from "../constants/Config";
 
 firebase.initializeApp(firebaseConfig);
 console.log("Firebase initialized");
-const dbInstance = db(firebase.firestore());
+const dbInstance = db(
+  firebase.firestore(),
+  prodCollectionName,
+  MaxVotes,
+  MinParticipants
+);
 console.log("Firebase instance created");
 
 export default dbInstance;
