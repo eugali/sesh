356a37978c7e68fa9b9fcba591233787e3caa47a
Eugenio Galioto <eu.galioto@gmail.com>
2021-05-02 20:16:50 +0200

    voting time reduced to 10 seconds

--- constants/Config.ts
+++ constants/Config.ts
@@ -12,6 +12,6 @@ export const baseURL = "https://sesh-e5398.web.app";
 
 export const prodCollectionName = "HMWs";
 export const SubmissionDuration = 30;
-export const VotingDuration = 15;
+export const VotingDuration = 10;
 export const MinParticipants = 1;
 export const MaxVotes = 4;
